288fa5f8a05f0e43d46f418e3814e066931e5144
Maciej <zorteran@gmail.com>
2017-11-24 10:38:06 +0100

    Implementacja pierwszego zadania

---
+++ Zadanie1/Zadanie1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj świecie!");
+            TryExit();
+        }
+
+        private static void TryExit()
+        {
+            string answer = "";
+            do
+            {
+                Console.WriteLine("Aby zamknąć aplikację wpisz 'exit'");
+                answer = Console.ReadLine();
+            } while (answer != "exit");
+        }
+    }
+}
