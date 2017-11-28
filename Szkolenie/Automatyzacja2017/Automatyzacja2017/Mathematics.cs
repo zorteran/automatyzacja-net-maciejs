da3a99e1408a5238150a1d078d5058c102816958
Maciej <zorteran@gmail.com>
2017-11-28 11:24:48 +0100

    added mathematics class for testing

---
+++ Szkolenie/Automatyzacja2017/Automatyzacja2017/Mathematics.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatyzacja2017
+{
+    public class Mathematics
+    {
+        public double Add(double x, double y)
+        {
+            return x + y;
+        }
+        public double Substract(double x, double y)
+        {
+            return x-y;
+        }
+        public double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+        public double Divide(double x, double y)
+        {
+            return x / y;
+        }
+    }
+}
