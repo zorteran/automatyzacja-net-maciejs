0784fc927ab0b49caa0b8e947b258ade57866a82
Maciej <zorteran@gmail.com>
2017-11-28 10:25:59 +0100

    created vs proj

---
+++ Szkolenie/Automatyzacja2017/Automatyzacja2017/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Automatyzacja2017
+{
+    public class Class1
+    {
+
+    }
+}
